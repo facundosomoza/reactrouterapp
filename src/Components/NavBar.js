48fc11d62cb65af1fbc31029d536a4afa3a58a1a
Facundo Somoza <fsomoza@gmail.com>
2022-09-09 12:23:46 +0100

    added cards and log-out

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,31 +5,51 @@ import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import { Link } from "react-router-dom";
 
-export default function NavBar() {
+export default function NavBar({ logUser, infoEmail, handleStateLogOut }) {
+  const handleLogOut = () => {
+    handleStateLogOut();
+  };
+
   return (
     <>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Router-App</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
+      <Row>
+        <Col>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">React-Router-App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                {!logUser ? (
+                  <>
+                    <Link className="nav-link" to="/register">
+                      Register
+                    </Link>
 
-            <Link className="nav-link" to="/login">
-              Log-in
-            </Link>
-
-            <Link className="nav-link" to="/userlist">
-              User List
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+                    <Link className="nav-link" to="/login">
+                      Log-in
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <Link className="nav-link" to="/userlist">
+                      User List
+                    </Link>
+                    <Link className="nav-link" to="/" onClick={handleLogOut}>
+                      Log-Out
+                    </Link>
+                    <div className="nav-link disabled">{infoEmail}</div>
+                  </>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Col>
+      </Row>
     </>
   );
 }
--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,5 +1,50 @@
-import React from "react";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
-export default function UserList() {
-  return <>UserList</>;
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
+export default function UserList({ app }) {
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const db = getFirestore(app);
+
+    const usersCollection = collection(db, "users");
+
+    const querySnapshot = await getDocs(usersCollection);
+
+    const newUsers = [];
+
+    querySnapshot.forEach((doc) => {
+      const userData = doc.data();
+
+      newUsers.push(userData);
+    });
+
+    setUsers(newUsers);
+  };
+
+  //Warning: useEffect must not return anything besides a function.
+  //como es una funcion async retorna una promise
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <Row>
+      {users.map((user) => (
+        <Col>
+          <Card className="card-link">
+            <Card.Body>
+              <Card.Title>{user.userName}</Card.Title>
+              <Card.Text>{user.userEmail}</Card.Text>
+              <Card.Text>{user.userPhone}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 }
